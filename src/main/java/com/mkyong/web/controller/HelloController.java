3534a18e5437b1293d98ca30b4a6edd99d13dd89
raju <34272479+rajsaplr@users.noreply.github.com>
2020-08-20 18:01:58 +0530

    Update HelloController.java

--- src/main/java/com/mkyong/web/controller/HelloController.java
+++ src/main/java/com/mkyong/web/controller/HelloController.java
@@ -18,7 +18,7 @@ public class HelloController {
 
 	}
 
-	@RequestMapping(value = "/hello/{name:.+}", method = RequestMethod.GET)
+	@RequestMapping(value = "/hello/{Raju:.+}", method = RequestMethod.GET)
 	public ModelAndView hello(@PathVariable("name") String name) {
 
 		ModelAndView model = new ModelAndView();
